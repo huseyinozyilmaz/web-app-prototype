f8ec6b66788f0c8f54bc19715f2d8ccdefe62de5
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2017-09-07 13:03:36 +0100

    Initial spring boot code

---
+++ src/main/java/org/huseyin/webappprototype/WebAppPrototypeApplication.java
@@ -0,0 +1,12 @@
+package org.huseyin.webappprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppPrototypeApplication.class, args);
+	}
+}
---
+++ src/test/java/org/huseyin/webappprototype/WebAppPrototypeApplicationTests.java
@@ -0,0 +1,16 @@
+package org.huseyin.webappprototype;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebAppPrototypeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
